a62727d32e8aa3ab207596a9ba3d1facd0096303
Joshua Matthews <jmatth93@gmail.com>
2016-12-23 22:57:56 -0600

    Initial commit

---
+++ Main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+  "log"
+  "net"
+  "os"
+)
+
+const (
+  CONN_HOST = "localhost"
+  CONN_PORT = "8080"
+  CONN_TYPE = "tcp"
+)
+
+func main() {
+  l, err := net.Listen(CONN_TYPE,CONN_HOST+":"+CONN_PORT)
+  if err != nil {
+    log.Fatal(err)
+  }
+  defer l.Close()
+  log.Println("Listening on "+CONN_HOST+":"+CONN_PORT)
+  // spin off group listener
+  for {
+
+  }
+}
